c588e98f7a58ebbb34b6ab6ff9cc087f7086b226
OfficialCodes101 <custofeng159@gmail.com>
2024-07-03 12:06:19 +0100

    changes

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ require("dotenv").config();
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"],
+  });
   const page = await browser.newPage();
 
   await page.goto("https://apexwealth.site/auth/signin");
+  console.log("Signing in");
 
   await page.type('input[name="email"]', process.env.EMAIL);
   await page.type('input[name="password"]', process.env.PASSWORD);
 
   await page.click('button[class="sign__btn"]');
+  console.log("Signed in");
 
   await page.waitForNavigation();
 
   await page.goto("https://apexwealth.site/profile/bonus");
 
   await page.click('button[data-id="claimBonus"]');
+  console.log("Successfully claimed");
 
   setTimeout(async () => {
     await browser.close();
